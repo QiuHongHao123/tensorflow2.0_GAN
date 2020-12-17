91494e0246ad64fcd93dfd349a1254a5cca6736b
Honghao Qiu <3158874848@qq.com>
2020-12-17 16:56:09 +0800

    models

--- test.py
+++ test.py
@@ -17,15 +17,15 @@ def preprocess(input):
     return out
 def test():
 
-    image_bytes_F = pydicom.dcmread('./test_f.dcm')
+    image_bytes_F = pydicom.dcmread('./L19-f-1-080.dcm')
     image_f = image_bytes_F.pixel_array
-    image_bytes_L = pydicom.dcmread('./test_l.dcm')
+    image_bytes_L = pydicom.dcmread('./L19-l-1-080.dcm')
     image_l = image_bytes_L.pixel_array
     image_l = preprocess(image_l)
     G = Generator_unet_d4()
 
     checkpoint = tf.train.Checkpoint(mymodel_G=G)
-    checkpoint.restore('./Models/withGAN-simplerD/ckpt-40')
+    checkpoint.restore('./Models/withGAN-l1loss-rate200/ckpt-15')
     plt.imshow(image_f,cmap='gray')
     plt.show()
     plt.imshow(image_l, cmap='gray')
